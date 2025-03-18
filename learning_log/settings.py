fe1014bcf7c6d7198047a6d82b9c1b6678204ebc
NickSheva <nikshevtsov@mail.ru>
2025-03-18 16:52:06 +0500

    add static

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,12 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
@@ -95,11 +90,10 @@ WSGI_APPLICATION = 'learning_log.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
